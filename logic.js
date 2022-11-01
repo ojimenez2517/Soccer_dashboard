ca6972e6949a27a192cb6d931d67efbdbea7aada
unknown <o6jimene@ucsd.edu>
2022-10-31 20:34:07 -0700

    added files for dashboard project

---
+++ logic.js
@@ -0,0 +1,301 @@
+let datadict = {
+    'LigaMX':{
+        'First':[
+            ['./Images/santos.png', './Images/leon.png', './Images/tigres.png', './Images/queretaro.png', './Images/necaxa.png'],
+            [6, 7, 5, 8, 4],
+            ['1. Santos', '2. Leon', '3. UNAL', '4. Queretaro', '5. Necaxa']
+        ],
+        'Second': [
+            ['Mexico', 364, '#006845', 'MEX'],
+            ['Argentina', 74, '#75AEE0', 'ARG'],
+            ['Colombia', 37, '#fcd20f', 'COL'],
+            ['Uruguay', 29, '#0035aa', 'URG'],
+            ['Chile', 21, '#d62718', 'CHI'],
+            ['Ecuador', 21, '#ffde00', 'ECU'],
+            ['Paraguay', 17, '#f0f0f0', 'PAR'],
+            ['Brazil', 9, '#009c37', 'BRA'],
+            ['United States', 9, '#002164', 'USA'],
+            ['Spain', 7, '#c60b1e', 'SPA'],
+            ['Peru', 6, '#e2a50a', 'PER'],
+            ['Other', 11, '#000000', 'OTHER']
+        ],
+        'Third': [
+            {
+                "values":['Edson Alvarez', 'Mexico', 'Ajax (Netherlands)', '$16.5m']
+            },
+            {
+                "values":['Uriel Atuna', 'Mexico', 'Chivas (Mexico)', '$10.89m']
+            },
+            {
+                "values":['Mateus Uribe', 'Colombia', 'FC Porto (Portugal)', '$10.45m']
+            },
+            {
+                "values":['Nicolas Lopez', 'Uruguay', 'TIgres (Mexico)', '$9.9m']
+            },
+            {
+                "values":['Vincent Janssen', 'Netherlands', 'Monterrey (Mexico)', '$9.9m']
+            }
+        ],
+        'Fourth': [
+            ['Santos', 'Leon', 'UNAL', 'Queretaro', 'Necaxa', 'America', 'Morelia', 'Monterrey', 'Pachuca', 'Guadalajara', 'Tijuana', 'Cruz Azul', 'Pumas', 'Atlas', 'Toluca', 'Puebla', 'Veracruz', 'Lobos BUAP'],
+            [14, 16, 12, 12, 11, 17, 13, 14, 14, 15, 18, 13, 15, 8, 2, 12, 14, 14]
+        
+        ]
+    },
+    'Superleague':{
+        'First':[
+            ['./Images/boca.png', './Images/river.png', './Images/velez.png', './Images/racing.png', './Images/argentinos.png'],
+            [1,2,1,0,1],
+            ['1. Boca Juniors', '2. River Plate', '3. Velez Sarsf', '4. Racing Club', '5. Argentinos Jun']
+        ],
+        'Second':[
+            ['Argentina', 738, '#75AEE0', 'ARG'],
+            ['Uruguay', 46, '#0035aa', 'URG'],
+            ['Paraguay', 33, '#f0f0f0', 'PAR'],
+            ['Colombia', 31, '#fcd20f', 'COL'],
+            ['Chile', 11, '#d62718', 'CHI'],
+            ['Venezuela', 4, '#fcd20f', 'VEN'],
+            ['Peru', 6, '#e2a50a', 'PER'],
+            ['Other', 13, '#000000', 'OTHER']
+        ],
+        'Third': [
+            {
+                "values":['Exequiel Palacios', 'Argentina', 'Leverkusen (Germany)', '$18.7m']
+            },
+            {
+                "values":['Nahitan Nández', 'Uruguay', 'Cagliari Calcio (Italy)', '$19.8m']
+            },
+            {
+                "values":['Darío Benedetto', 'Argentina', 'Marseille (France)', '$15.4m']
+            },
+            {
+                "values":['Cristian Pavón', 'Argentina', 'La Galaxy (USA)', '$1.96m']
+            },
+            {
+                "values":['Nicolás Domínguez', 'Argentina', 'Bologna (Italy)', '$8.09m']
+            }
+        ],
+        'Fourth': [
+            ['Boca Juniors', 'River Plate', 'Velez Sarsf', 'Racing Club', 'Argentinos Jun', 'Defensa y Just', 'Lanus', 'San Lorenzo', 'Rosario Cent', "Newell's OB", 'Talleres', 'Estudiantes', 'Independiente', 'Tucuman', 'CA Union', 'Banfield', 'Gimnasia ELP', 'Patronato', 'CA Huracan', 'Aldosivi', 'Colon', 'Godoy Cruz'],
+            [4, 4, 2, 3, 2, 8, 7, 4, 6, 7, 7, 3, 5, 4, 6, 5, 4, 6, 4, 5, 8, 6]
+        ]
+    },
+    'Ligue1':{
+        'First':[
+            ['./Images/flamengo.png', './Images/santos_bra.png', './Images/palme.png', './Images/gremio.png', './Images/para.png'],
+            [1,2,1,0,1],
+            ['1. Flamengo', '2. Santos', '3. Palmeiras', '4. Gremio', '5. Atl Paranaense']
+        ],
+        'Second':[
+            ['Brazil', 754, '#009c37', 'BRA'],
+            ['Argentina', 19, '#75AEE0', 'ARG'],
+            ['Colombia', 17, '#fcd20f', 'COL'],
+            ['Paraguay', 11, '#f0f0f0', 'PAR'],
+            ['Uruguay', 9, '#0035aa', 'URG'],
+            ['Peru', 4, '#e2a50a', 'PER'],
+            ['Ecuador', 3, '#ffde00', 'ECU'],
+            ['Venezuela', 3, '#fcd20f', 'VEN'],
+            ['Other', 7, '#000000', 'OTHER']
+        ],
+        'Third': [
+            {
+                "values":['Rodrygo', 'Brazil', 'Real Madrid (Spain)', '$49.5m']
+            },
+            {
+                "values":['Reinier', 'Brazil', 'Real Madrid (Spain)', '$33m']
+            },
+            {
+                "values":['Gabriel Barbosa', 'Brazil', 'Flamengo (Brazil)', '$19.2m']
+            },
+            {
+                "values":['Joao Pedro', 'Brazil', 'Watford (England)', '$4.4m']
+            },
+            {
+                "values":['Bruno Guimaraes', 'Brazil', 'Lyon (France)', '$22m']
+            }
+        ],
+        'Fourth': [
+            ['Flamengo', 'Santos', 'Palmeiras', 'Gremio', 'Atl Paranaense', 'Sai Paulo', 'International', 'Corinthians', 'Fortaleza', 'Goias', 'Bahia', 'Vasco da Gamma', 'Atletico Mineiro', 'Fluminense', 'Botafogo', 'Ceara', 'Cruzeiro', 'CSA', 'Chapecoense', 'Avai'],
+            [4, 3, 1, 6, 4, 7, 3, 1, 1, 0, 2, 4, 2, 3, 8, 1, 6, 2, 2, 7]
+        ]
+    }
+};
+
+let colors = {
+    'LigaMX': '#a61708',
+    'Superleague': '#95CEF0',
+    'Ligue1': '#007c17'
+};
+
+function makeItem(name){
+    let dict = datadict[name]
+    let col = colors[name]
+    Highcharts.chart('itemChart', {
+        chart:{
+            type: 'item',
+            backgroundColor: col
+        },
+        title:{
+            text: 'Nationalies in League',
+            style: {
+                color: "#FFFFFF"
+            }
+        },
+        subtitle:{
+            text: 'Player vizualization',
+            style: {
+                color: "#DFDFDF"
+            }
+        },
+        legend: {
+            labelFormat: '{name} <span style="opacity: 0.4">{y}</span>',
+            itemStyle: {
+                color: "#FFFFFF"
+            }
+        },
+        series: [{
+            name: 'Player Count',
+            keys: ['name', 'y', 'color', 'label'],
+            data: dict['Second'],
+            dataLabels:{
+                enabled: false,
+                format: '{point.label}'
+            },
+
+            //Circle options
+            center: ['50%', '50%'],
+            size: '120%',
+            startAngle: 0,
+            endAngle: 0,
+            borderColor: col
+        }],
+    })
+}
+
+function makeBar(name){
+    let dict = datadict[name];
+    let col = colors[name]
+    Highcharts.chart('bar', {
+        chart: {
+            type: 'column',
+            height: '30%',
+            backgroundColor: col
+        },
+        title: {
+            text : 'International Players per Squad',
+            style: {
+                color: "#FFFFFF"
+            }
+        },
+        xAxis: {
+            categories: dict['Fourth'][0],
+            title:{
+                text:'Squad',
+                style: {
+                    color: "#FFFFFF"
+                }
+            },
+            labels:{
+                style: {
+                    color: "#FFFFFF"
+                }
+            }
+        },
+        yAxis:{
+            title:{
+                text: '# Nondomestic Players',
+                style: {
+                    color: "#FFFFFF"
+                }
+            },
+            labels:{
+                style: {
+                    color: "#FFFFFF"
+                }
+            }
+        },
+        legend: {
+            itemStyle: {
+                color: "#FFFFFF"
+            }
+        },
+        series: [{
+            name: 'Players',
+            color: '#FFFFFF',
+            data: dict['Fourth'][1]
+        }]
+    })
+}
+
+
+function openTab(name, t, col){
+    let tabcontents = document.getElementsByClassName('tabval');
+    for(let i=0; i<tabcontents; i++){
+        tabcontents[i].getElementsByClassName.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    }
+
+    vizCreator(name)
+    document.getElementById('LigaMX').style.display = "block";
+    t.style.backgroundColor = col;
+    document.getElementById("LigaMX").style.backgroundColor = col;
+    
+}
+
+
+function vizCreator(n){
+    let dict = datadict[n];
+
+    let col = colors[n]
+
+    let teams = document.getElementsByClassName('teamPic');
+    let texts = document.getElementsByClassName('teamTxt');
+    let names = document.getElementsByClassName('teamName');
+    
+    for(let i=0; i < 5; i++){
+        team = teams[i];
+        text = texts[i];
+        teamName = names[i];
+        
+        team.setAttribute('href', dict['First'][0][i]);
+        text.innerHTML = dict['First'][1][i]
+        teamName.innerHTML = dict['First'][2][i]
+    }
+
+    let gridConfig = {
+        "graphset":[
+            {
+                "type":"grid",
+                "backgroundColor": col,
+                "title":{
+                    "text":"Most Valuable Transfers",
+                    "color":"#FFFFFF"
+                },
+                "plotarea":{
+                    "margin": "35 10 30 50"
+                },
+                "options":{
+                    "col-labels":['Name', 'Nationality', 'Transfer To', 'Transfer Value ($)']
+                },
+                "series": dict['Third']
+            }
+        ]
+    };
+
+
+    zingchart.render({
+        id : 'grid',
+        data: gridConfig,
+        height: '76%',
+        width: '95%'
+    });
+    makeItem(n);
+    makeBar(n);
+}
+
+
+document.getElementById("Mex").click();
